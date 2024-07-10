5b029803fc08c120cbbe4a6caa9a548ffa7ccdae
MoBazmul <mohaarash001@gmail.com>
2024-07-10 23:29:33 +0300

    Modify th redirect in the login page

--- studylink/routes.py
+++ studylink/routes.py
@@ -98,20 +98,19 @@ def login():
   User login view.
   """
   if current_user.is_authenticated:
-    return redirect(url_for('home'))
+      return redirect(url_for('home'))
   
   form = LoginForm()
-
   if form.validate_on_submit():
-    user = User.query.filter_by(email=form.email.data).first()
+      user = User.query.filter_by(email=form.email.data).first()
 
-    if user and bcrypt.check_password_hash(user.password, form.password.data):
-      login_user(user, remember=form.remember.data)
-      next_page = request.args.get('next')
-      return redirect(next_page) if next_page else redirect(url_for('home'))
-    else:
-      flash('Login Unsuccessfull. Please check email and password', 'danger')
-  
+      if user and bcrypt.check_password_hash(user.password, form.password.data):
+          login_user(user, remember=form.remember.data)
+          next_page = request.args.get('next')
+          return redirect(next_page or url_for('home'))
+      else:
+          flash('Login unsuccessful. Please check email and password.', 'danger')
+          
   return render_template('login.html', title='Login', form=form)
 
 
