6168670f1dcea4c6cf8fbfd194ce06c652369c75
MoBazmul <mohaarash001@gmail.com>
2024-07-11 20:56:06 +0300

    Adding a search bar in the home page

--- studylink/routes.py
+++ studylink/routes.py
@@ -72,6 +72,25 @@ def home():
   return render_template("home.html", title="Home", resources=resources, is_authenticated=is_authenticated, current_time=datetime.utcnow())
 
 
+@app.route('/search', methods=['GET'])
+def search_resources():
+  query = request.args.get('query', '')
+  
+  resources = Resources.query.filter(
+      Resources.title.contains(query) | 
+      Resources.description.contains(query)
+  ).all()
+
+  resources_dict = {}
+  for resource in resources:
+      course_name = resource.course_name
+      if course_name not in resources_dict:
+          resources_dict[course_name] = []
+      resources_dict[course_name].append(resource)
+
+  return render_template('home.html', resources_dict=resources_dict)
+
+
 
 @app.route("/register", methods=['POST', 'GET'])
 def register():
